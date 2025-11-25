bee03fe62faa7466ccfdc48a57ef808d83a387b9
Suyogya Ratna Tamrakar <suyogya.tamrakar@fonenxt.com>
2025-11-25 23:41:37 +0545

    Initial Commit

---
+++ multilingual-keyboardUITests/multilingual_keyboardUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  multilingual_keyboardUITestsLaunchTests.swift
+//  multilingual-keyboardUITests
+//
+//  Created by Suyogya Ratna Tamrakar on 25/11/25.
+//
+
+import XCTest
+
+final class multilingual_keyboardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
